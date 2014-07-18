a48d272e943a3cb013058f4432c5688137569f27
osk <oskosk@gmail.com>
2014-07-18 10:56:07 -0300

    Followed auth setup instructions using password. Instructiones taken from https://github.com/kasperisager/sails-generate-auth

--- config/routes.js
+++ config/routes.js
@@ -30,7 +30,17 @@ module.exports.routes = {
   '/': {
     view: 'homepage'
   },
+  // Following https://github.com/kasperisager/sails-generate-auth/ instructions
 
+  'get /login': 'AuthController.login',
+  'get /logout': 'AuthController.logout',
+  'get /register': 'AuthController.register',
+
+  'post /auth/local': 'AuthController.callback',
+  'post /auth/local/:action': 'AuthController.callback',
+
+  'get /auth/:provider': 'AuthController.provider',
+  'get /auth/:provider/callback': 'AuthController.callback',
 
   // Custom routes here...
 
