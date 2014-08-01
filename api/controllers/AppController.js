e21b94c899e640a59c758931804c45254f6d65d3
Christian Gastrell <cgastrell@gmail.com>
2014-08-01 12:58:09 -0300

    merge con los cambios de tomy

--- api/controllers/AppController.js
+++ api/controllers/AppController.js
@@ -7,128 +7,123 @@
 "use strict";
 
 module.exports = {
-    listMyApps: function(req, res) {
-        User.findOne(req.session.user.id)
-            .populate('ownApps')
-            .exec(function(err, user) {
-                res.view({
-                    userdata: user
-                });
-            });
-    },
-    newAppForm: function(req, res) {
-        res.view();
-    },
-    createAppEx: function(req, res) {
-        // console.log(req.allParams());
-        // return;
-        App.create({
-            name: req.param('name'),
-            description: req.param('description'),
-            owner: req.session.user.id
-        }, function appCreated(err, app) {
-            if (err) {
-                req.session.flash = {
-                    err: err
-                }
-                return res.redirect('/apps/new');
-            }
-            // res.json(app);
-            app.save(function(err) {
-                console.log(err);
-                return res.redirect('/apps/new');
-            });
-            res.redirect('/apps/' + app.id + '/edit');
+  listMyApps: function(req, res) {
+    User.findOne(req.session.user.id)
+      .populate('ownApps')
+      .exec(function(err, user) {
+        res.view({
+          userdata: user
         });
+      });
+  },
+  newAppForm: function(req, res) {
+    res.view();
+  },
+  createAppEx: function(req, res) {
+    // console.log(req.allParams());
+    // return;
+    App.create({
+      name: req.param('name'),
+      description: req.param('description'),
+      owner: req.session.user.id
+    }, function appCreated(err, app) {
+      if (err) {
+        req.session.flash = {
+          err: err
+        }
+        return res.redirect('/apps/new');
+      }
+      // res.json(app);
+      app.save(function(err) {
+        console.log(err);
+        return res.redirect('/apps/new');
+      });
+      res.redirect('/apps/' + app.id + '/edit');
+    });
 
-    },
-    viewApp: function(req, res) {
-        App.findOne(req.param('id'), function appFound(err, app) {
-            res.view({
-                appdata: app
-            });
+  },
+  viewApp: function(req, res) {
+    App.findOne(req.param('id'), function appFound(err, app) {
+      res.view({
+        appdata: app
+      });
+    });
+  },
+  //rest - json only actions --cg
+  /**
+   * Adds a plugin (by id) to the app plugins collection (app.plugins)
+   *
+   * App MUST be owned by logged in user
+   *
+   * @param app_id
+   * @param plugin_id
+   *
+   * @todo chequear que el plugin PUEDA ser agregado (esta pago o es free)
+   */
+  attachPlugin: function(req, res) {
+    var appId = req.param('id');
+    var pluginId = req.param('plugin_id');
+    App.findOne(appId)
+      .populate('plugins')
+      .exec(function appFound(err, app) {
+        app.plugins.add(pluginId);
+        app.save(function(err) {
+          if (err) {
+            return res.json(err);
+          }
         });
-    },
-    //rest - json only actions --cg
-    /**
-     * Adds a plugin (by id) to the app plugins collection (app.plugins)
-     *
-     * App MUST be owned by logged in user
-     *
-     * @param app_id
-     * @param plugin_id
-     *
-     * @todo chequear que el plugin PUEDA ser agregado (esta pago o es free)
-     */
-    attachPlugin: function(req, res) {
-        var appId = req.param('id');
-        var pluginId = req.param('plugin_id');
-        App.findOne(appId)
-            .populate('plugins')
-            .exec(function appFound(err, app) {
-                app.plugins.add(pluginId);
-                app.save(function(err) {
-                    if (err) {
-                        return res.json(err);
-                    }
-                });
-                return res.json(app.toObject());
-            });
-    },
-    /**
-     * Creates an app
-     *
-     * app.owner is set by logged in user
-     *
-     * params according to models/app
-     *
-     */
-    createApp: function(req, res) {
-        var attrs = req.allParams();
-        App.create(attrs).done(function appCreated(err, app) {
-            if (err)
-                return res.json(err);
+        return res.json(app.toObject());
+      });
+  },
+  /**
+   * Creates an app
+   *
+   * app.owner is set by logged in user
+   *
+   * params according to models/app
+   *
+   */
+  createApp: function(req, res) {
+    var attrs = req.allParams();
+    App.create(attrs).done(function appCreated(err, app) {
+      if (err)
+        return res.json(err);
 
-            res.json(app);
-        });
-        // console.log(attrs);
-        // res.json(attrs);
-    },
-    updateApp: function(req, res) {
+      res.json(app);
+    });
+    return res.json(app.toObject());
+  },
+  updateApp: function(req, res) {
 
-    },
-    destroyApp: function(req, res) {
+  },
+  destroyApp: function(req, res) {
 
-    },
-    listAttachedPlugins: function(req, res) {
+  },
+  listAttachedPlugins: function(req, res) {
 
-    },
-    viewPluginOptions: function(req, res) {
+  },
+  viewPluginOptions: function(req, res) {
 
-    },
-    updatePluginOptions: function(req, res) {
+  },
+  updatePluginOptions: function(req, res) {
 
-    },
-    deattachPlugin: function(req, res) {
+  },
+  deattachPlugin: function(req, res) {
 
-    },
-    viewTemplate: function(req, res) {
+  },
+  viewTemplate: function(req, res) {
 
-    },
-    attachTemplate: function(req, res) {
+  },
+  attachTemplate: function(req, res) {
 
-    },
-    viewTemplateOptions: function(req, res) {
+  },
+  viewTemplateOptions: function(req, res) {
 
-    },
-    updateTemplateOptions: function(req, res) {
+  },
+  updateTemplateOptions: function(req, res) {
 
-    },
-    deattachTemplate: function(req, res) {
+  },
+  deattachTemplate: function(req, res) {
 
-    },
-
-
-
-
+  }
 };
