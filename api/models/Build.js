806149341143b4a31672d6c909b1dd6a0f61e4b7
marcote <tomas.marcote@gmail.com>
2014-08-01 14:39:39 -0300

    agregue arhivos de build a git

---
+++ api/models/Build.js
@@ -0,0 +1,27 @@
+/**
+ * Build.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+    app: {
+      model: 'App'
+    },
+    buildNumber: {
+      type: 'string'
+    },
+    android: {
+      type: 'string'
+    },
+    ios: {
+      type: 'string'
+    },
+    winphone: {
+      type: 'string'
+    }
+  }
+};
