a9ad0189b3ca43acb0327795135a92475703338b
marcote <tomas.marcote@gmail.com>
2014-08-04 18:40:58 -0300

    Se agregaro pluginOptions

---
+++ api/controllers/PluginOptionsController.js
@@ -0,0 +1,11 @@
+/**
+ * PluginOptionsController
+ *
+ * @description :: Server-side logic for managing pluginoptions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/PluginOptions.js
@@ -0,0 +1,21 @@
+/**
+ * PluginOptions.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+    app: {
+      model: 'App'
+    },
+    plugin: {
+      model: 'Plugin'
+    },
+    parent: {
+      type: 'string'
+    }
+  }
+};
