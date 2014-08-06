388daf76a5bf1db4dbaf1fe2841798b3f774cc8e
Christian Gastrell <cgastrell@gmail.com>
2014-08-06 11:47:37 -0300

    metodos en templateController

--- api/controllers/TemplateController.js
+++ api/controllers/TemplateController.js
@@ -12,8 +12,31 @@ module.exports = {
   find: function(req, res) {
 
   },
+  /**
+   * Creates a template
+   *
+   * template.owner is set by logged in user
+   *
+   * @returns object created template
+   * @method POST
+   * @endpoint /template
+   * @todo falta validar y controlar/asegurar los campos/attributes
+   * @todo needs testing
+   */
   create: function(req, res) {
-
+    var attrs = req.allParams();
+    Template.create(attrs, function templateCreated(err, tpl) {
+      if (err) {
+        return res.json(err);
+      }
+      tpl.owner = req.session.user.id;
+      tpl.save(function tplSaveError(err) {
+        if (err) {
+          return res.json(err);
+        }
+        res.json(tpl.toObject());
+      })
+    });
   },
   findOne: function(req, res) {
 
